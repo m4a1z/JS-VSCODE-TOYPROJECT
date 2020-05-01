6217f8b2f84bf03cf592b4e1aff028347d777832
unknown <m4a1z89@naver.com>
2020-05-01 17:15:00 +0900

    3.1연습

---
+++ 3-1.js
@@ -0,0 +1,26 @@
+'use script'
+
+//숫자 타입
+var intNum  = 10;
+var floatNum = 0.1;
+
+//문자열 타입
+
+var singleQuotesStr = 'nice';
+var doubleQuotesStr = "doubleNice";
+var singleChar = 'a';
+
+//불린타입
+var boolVar = true;
+
+// null타입
+var nullVar  = null;
+
+console.log(
+    typeof intNum  ,
+    typeof floatNum ,
+    typeof singleQuotesStr ,
+    typeof doubleQuotesStr ,
+    typeof singleChar , 
+    typeof boolVar ,
+    typeof nullVar  );
